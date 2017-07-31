92cfa943511bf712b403bb72b170c01a7bd21626
fankay <fankai@kaishengit.com>
2017-07-31 16:04:19 +0800

    演示Hibernate的Pojo生命周期以及HQL查询

---
+++ hibernate/src/test/java/com/kaishengit/hibernate/HQLTest.java
@@ -0,0 +1,130 @@
+package com.kaishengit.hibernate;
+
+import com.kaishengit.pojo.Account;
+import com.kaishengit.util.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class HQLTest {
+
+    private Session session;
+
+    @Before
+    public void before() {
+        session = HibernateUtil.getSession();
+        session.beginTransaction();
+    }
+
+    @After
+    public void after() {
+        session.getTransaction().commit();
+    }
+
+    @Test
+    public void findAll() {
+
+        String hql = "from Account";
+        Query query = session.createQuery(hql);
+        List<Account> accountList = query.list();
+
+        for(Account account : accountList) {
+            System.out.println(account.getId() + " -> " + account.getUsername() + " -> " + account.getAddress());
+        }
+    }
+
+    @Test
+    public void findByUserName() {
+        String hql = "from Account as ac where ac.username = :name";
+
+        Query query = session.createQuery(hql);
+
+        //query.setString(0,"王思雨");
+        //query.setParameter(0,"王思雨");
+        query.setParameter("name","王思雨");
+
+        List<Account> accountList = query.list();
+
+        for(Account account : accountList) {
+            System.out.println(account.getId() + " -> " + account.getUsername() + " -> " + account.getAddress());
+        }
+    }
+
+
+    @Test
+    public void findBy() {
+        String hql = "select id,username,address from Account where age = :age";
+        Query query = session.createQuery(hql);
+        query.setParameter("age",23);
+
+        List<Object[]> accountList = query.list();
+
+        for(Object[] array : accountList) {
+            System.out.println(array[0] + " -> " + array[1] + " -> " + array[2]);
+        }
+    }
+
+    @Test
+    public void count() {
+
+        String hql = "select count(*),max(id) from Account";
+        Query query = session.createQuery(hql);
+
+        Object[] objects = (Object[]) query.uniqueResult();
+        System.out.println("count:" + objects[0]);
+        System.out.println("max:" + objects[1]);
+
+       /* Long count = (Long) query.uniqueResult();
+        System.out.println("count:" + count);*/
+
+       /* List<Long> count = query.list();
+        System.out.println(count.get(0));*/
+    }
+
+
+    @Test
+    public void findByUserNameDesc() {
+        String hql = "from Account as ac where ac.username = :name order by id desc";
+
+        Query query = session.createQuery(hql);
+
+        //query.setString(0,"王思雨");
+        //query.setParameter(0,"王思雨");
+        query.setParameter("name","王思雨");
+
+        List<Account> accountList = query.list();
+
+        for(Account account : accountList) {
+            System.out.println(account.getId() + " -> " + account.getUsername() + " -> " + account.getAddress());
+        }
+    }
+
+    @Test
+    public void findByUserNameDescPage() {
+        String hql = "from Account as ac where ac.username = :name order by id desc";
+
+        Query query = session.createQuery(hql);
+
+        //query.setString(0,"王思雨");
+        //query.setParameter(0,"王思雨");
+        query.setParameter("name","王思雨");
+
+        //limit 4,4  分页
+        query.setFirstResult(4);
+        query.setMaxResults(4);
+
+        List<Account> accountList = query.list();
+
+        for(Account account : accountList) {
+            System.out.println(account.getId() + " -> " + account.getUsername() + " -> " + account.getAddress());
+        }
+    }
+
+
+
+
+}
--- hibernate/src/test/java/com/kaishengit/hibernate/Test.java
+++ hibernate/src/test/java/com/kaishengit/hibernate/Test.java
@@ -9,6 +9,7 @@ import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+import org.junit.Assert;
 
 import java.util.List;
 
@@ -34,27 +35,91 @@ public class Test {
         account.setAddress("北京");
         account.setAge(23);
 
-        session.save(account);
+        session.save(account);//merge=saveOrUpdate 不改变对象状态
+
+        //session.clear(); //将session关联的所有对象从session中清除
+
+        account.setUsername("王老五");
+        session.flush();
 
 
         //关闭事务（提交 | 回滚）
-        transaction.commit();
+        transaction.commit();// session.close();
 
+    }
 
+    @org.junit.Test
+    public void updateAccount() {
 
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        Account account = (Account) session.get(Account.class,6); //持久态
+
+        session.getTransaction().commit(); // 游离态
+
+        Session session2 = HibernateUtil.getSession();
+        session2.beginTransaction();
+
+        account.setUsername("李老八"); //游离态
+        session2.merge(account); // 再次 持久态
+
+        session2.getTransaction().commit(); //再次 游离态
 
 
     }
+
+
+
+    @org.junit.Test
+    public void save() {
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        Account account = new Account();
+        account.setUsername("王思雨");
+        account.setAddress("北京");
+        account.setAge(23);
+
+        session.persist(account);
+        System.out.println(account.getId());
+
+
+        session.getTransaction().commit();
+    }
+
 
     @org.junit.Test
     public void findById() {
         Session session = HibernateUtil.getSession();
         session.getTransaction().begin();
 
-        Account account = (Account) session.get(Account.class,1);
-        System.out.println(account.getUsername());
+        Account account = (Account) session.get(Account.class,22);
+        //System.out.println(account.getUsername());
         session.getTransaction().commit();
+
+        Assert.assertNull(account);
+
     }
+
+    @org.junit.Test
+    public void findByIdWithLoad() {
+        Session session = HibernateUtil.getSession();
+        session.getTransaction().begin();
+
+        Account account = (Account) session.load(Account.class,22);
+        //System.out.println(account.getUsername());
+
+        System.out.println(account.getUsername());
+
+        session.getTransaction().commit();
+
+
+
+        //System.out.println(account.getUsername());
+    }
+
+
 
     @org.junit.Test
     public void update() {
